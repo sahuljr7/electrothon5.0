112fe1b21ea0d2a78989b5c43ed1e9ddcb7286ea
sahuljr7 <sahulkumarparida@gmail.com>
2023-04-09 10:03:59 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Sahul Kumar Parida</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} SKP</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/sahuljr7"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/sahul_22_jr"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://linkedin.com/in/sahulkumarparida"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://instagram.com/sahul_22_jr"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import crowdfunding from "../../Assets/Projects/crowd.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          Electrothon <strong className="purple">5.0 </strong>
+        </h1>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={crowdfunding}
+              isBlog={false}
+              title="Decentralized Crowdfunding Platform"
+              description="A decentralized crowdfunding platform using blockchain is a platform where projects and businesses can raise funds without the need for intermediaries. The platform operates on a decentralized network of computers that record and verify all transactions. 
+              The platform uses smart contracts, which are self-executing contracts with the terms of the agreement directly written into code. Smart contracts are used to automate the fund allocation process and ensure that funds are only released when specific conditions are met, providing transparency and accountability to the crowdfunding process.
+              "
+              ghLink="https://github.com/sahuljr7/electrothon5.0-nith"
+              demoLink="https://crowdfunding-sahuljr7.netlify.app/"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
