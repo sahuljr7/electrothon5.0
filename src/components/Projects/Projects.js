3f3998c2e6be57695e8255d42602a4cc5db60bd9
Sahul Kumar Parida <71061738+sahuljr7@users.noreply.github.com>
2023-04-09 10:31:26 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,7 +21,7 @@ function Projects() {
               description="A decentralized crowdfunding platform using blockchain is a platform where projects and businesses can raise funds without the need for intermediaries. The platform operates on a decentralized network of computers that record and verify all transactions. 
               The platform uses smart contracts, which are self-executing contracts with the terms of the agreement directly written into code. Smart contracts are used to automate the fund allocation process and ensure that funds are only released when specific conditions are met, providing transparency and accountability to the crowdfunding process.
               "
-              ghLink="https://github.com/sahuljr7/electrothon5.0-nith"
+              ghLink="https://github.com/sahuljr7/electrothon5.0"
               demoLink="https://crowdfunding-sahuljr7.netlify.app/"
             />
           </Col>
